44df39ba4882608dcc7539dfe0a20966e63c198e
niceonionslice <niceonionslice@gmail.com>
2019-03-16 00:00:35 +0900

    update users_controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,10 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only[:show, :update, :destroy, :edit]
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -12,13 +18,23 @@ class Admin::UsersController < ApplicationController
     end
   end
 
-  def edit
-  end
-
   def show
   end
 
-  def index
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
+    else
+      render new
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}」を削除しました。"
   end
 
   private
@@ -26,4 +42,10 @@ class Admin::UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+
+  end
+
 end
