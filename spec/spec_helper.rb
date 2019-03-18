7ee9b46b0c84938c9966c21ab0f60241d8a8c875
niceonionslice <niceonionslice@gmail.com>
2019-03-18 16:28:22 +0900

    Capybaraの初期準備

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,20 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+# RSpecでCapybaraを扱うために必要な機能を読み込む
+require 'capybara/rspec'
+
+
 RSpec.configure do |config|
+  # SystemSpecを実行するドライブの設定をします。処理を高速にするためにHeadlessブラウザーを設定します。
+  config.before(:each, type: :system) do
+    driven_by :selenium_chrome_headless
+  end
+
+
+
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
