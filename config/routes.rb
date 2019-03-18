02830084c1d7e8de8d01ea31122503b828298360
niceonionslice <niceonionslice@gmail.com>
2019-03-18 10:45:13 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  get   '/login', to: 'sessions#new'
+  post  '/login', to: 'sessions#create'
+  post  '/logout', to: 'sessions#destroy'
   namespace :admin do
     resources :users
   end
