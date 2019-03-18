500ffd552feec437b1f8ec20637e7219b78ff55d
niceonionslice <niceonionslice@gmail.com>
2019-03-18 12:17:29 +0900

    update authenticate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,23 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: session_params[:email])
+
+    if user&.authenticate(session_params[:password])
+      session[user_id] = user.id
+      redirect_to root_path, notice: 'ログインしました'
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def sesson_params
+    params.require(:session).permit(:email, :password)
+
   end
 end
