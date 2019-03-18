e3e633b2d72bbf0fe978703bd852577efc89de04
niceonionslice <niceonionslice@gmail.com>
2019-03-18 13:37:29 +0900

    update migration add user_id to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_13_150439) do
+ActiveRecord::Schema.define(version: 2019_03_18_043346) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "タスクを管理するためのテーブル", force: :cascade do |t|
     t.string "name", null: false, comment: "タスクの名称"
