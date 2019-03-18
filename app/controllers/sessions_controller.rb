46d809ae5f38ebbc7dcdd3592af92284fd908a57
niceonionslice <niceonionslice@gmail.com>
2019-03-18 12:34:59 +0900

    typo fix codemiss! 😢

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: session_params[:email])
 
     if user&.authenticate(session_params[:password])
-      session[user_id] = user.id
+      session[:user_id] = user.id
       redirect_to root_path, notice: 'ログインしました。'
     else
       render :new
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
 
   private
 
-  def sesson_params
+  def session_params
     params.require(:session).permit(:email, :password)
 
   end
