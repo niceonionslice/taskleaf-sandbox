6cf2bbcbaf1b0c8629463fa50783a897255a989f
niceonionslice <niceonionslice@gmail.com>
2019-04-05 17:21:29 +0900

    ページネーションのデザインにbootstrap4を適用

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,6 +6,8 @@ class TasksController < ApplicationController
     # @tasks = current_user.tasks
     @q = current_user.tasks.ransack(params[:q])
     @tasks = @q.result(distinct: true).page(params[:page])
+    # kaminariの表示件数のdefaultを変更するときはpreメソッドを利用するといい。
+    # @tasks = @q.result(distinct: true).page(params[:page]).per(50)
   end
 
   # 詳細
